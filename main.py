a0f73b16b0eeeeb3d9b0dbff142421846cf79bf7
Firandos <douglas@techni.ca>
2025-02-17 12:03:03 -0500

    Simple dice and player movement along the board, move tile position to the tile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 import boardClass
+import playerClass
+import ui
 
 def main():
     pygame.init()
@@ -13,9 +15,10 @@ def main():
     img = pygame.transform.scale(pygame.image.load("Resources\\gunsalute-scarlets-mckenzie.jpg"),(640,480))
     
     #initialize board
-    tileList = ["Type1", "Type2", "Type1", "Type2", "Type3","Type1", "Type2", "Type1", "Type2", "Type3"]
+    tileList = [("Type1", 5, 15), ("Type2", 15, 15), ("Type1", 25, 15), ("Type2", 35, 15), ("Type3", 45, 15),
+                ("Type1", 5, 25), ("Type2", 15, 25), ("Type1", 25, 25), ("Type2", 35, 25), ("Type3", 45, 25)]
     board = boardClass.Board(tileList)
-    print(board.playerTile(4))
+
 
     # Main loop
     running = True
@@ -23,9 +26,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                ui.diceClick(screen, board)
 
         screen.blit(img, (0,0))  # Fill screen with image
         board.drawBoard(screen)
+        ui.diceButton(screen)
         pygame.display.flip()
 
     pygame.quit()
