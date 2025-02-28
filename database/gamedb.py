be298a5ad415ec8f70d33ef6dd1bfd25198d2fca
lamesgee <s30168@rmc-cmr.ca>
2025-02-28 08:45:35 -0500

    WIP update_stats and get_stat

--- database/gamedb.py
+++ database/gamedb.py
@@ -10,11 +10,11 @@ class GameDataBase(object):
             cls.instance = super(GameDataBase, cls).__new__(cls)
             return cls.instance
 
-    def get_connection(self):
+    def _get_connection(self):
         return sqlite3.connect(DB_NAME)
 
     def initialize_db(self):
-        conn = self.get_connection()
+        conn = self._get_connection()
         cursor = conn.cursor()
         cursor.execute(
             """
@@ -23,6 +23,13 @@ class GameDataBase(object):
             name TEXT NOT NULL,
             position INTEGER
             )
+        
+        CREATE TABLE iF NOT EXISTS stats (
+            player_id INTEGER FOREIGN KEY REFERENCES players(id) 
+            military INTEGER,
+            bilingualism INTEGER,
+            fitness INTEGER,
+            academics INTEGER
         """
         )
 
@@ -30,18 +37,19 @@ class GameDataBase(object):
         conn.close()
 
     def create_player(self, name: str) -> None:
+        # also save the stats
         """Create player with given name.
 
         Args:
             name (str): name of the players
         """
-        conn = self.get_connection()
+        conn = self._get_connection()
         conn.cursor().execute(f"INSERT INTO players (name) VALUES ('{name}')")
         conn.commit()
         conn.close()
 
     def get_player(self, player_id: int) -> Any:
-        conn = self.get_connection()
+        conn = self._get_connection()
         cursor = conn.cursor()
         cursor.execute(f"SELECT id, name, position FROM players WHERE id = {player_id}")
         row = cursor.fetchone()
@@ -50,7 +58,7 @@ class GameDataBase(object):
         return row
 
     def update_player_position(self, player_id, new_position) -> None:
-        conn = self.get_connection()
+        conn = self._get_connection()
         cursor = conn.cursor()
 
         cursor.execute(
@@ -58,3 +66,35 @@ class GameDataBase(object):
         )
         conn.commit()
         conn.close()
+
+    # update stat
+    def update_stats (self, player_id: int, military=0, bilingualism=0, fintess=0, academics=0): 
+        conn = self._get_connection()
+        cursor = conn.cursor()
+
+        curr_stat = get_stat()
+
+
+
+        
+        curosor.execute()
+
+
+    # get stat
+    def get_stat(self, player_id: int):
+        """
+
+        Args:
+            player_id (int): player id 
+
+        Returns:
+            row 
+        """
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT id, military, bilingualism, fitness, academics FROM players WHERE id = {player_id}")
+        row = cursor.fetchone()
+        conn.close()
+
+        return row
+        
