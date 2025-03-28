af7ada531e27b7fc58ba97f47fc4e5b3a9b705cf
Firandos <douglas@techni.ca>
2025-03-28 12:10:54 -0400

    I blame the lack of adult supervision

--- ui.py
+++ ui.py
@@ -56,9 +56,11 @@ class UI():
         self.message = None  # Variable to store the current message
         self.message_duration = 0  # Number of frames the message will stay on screen
         self.sounds = dict(click=pygame.mixer.Sound("Resources/sounds/click.ogg"),
-                           start=pygame.mixer.Sound("Resources/sounds/I am notta da mario.ogg"))
+                           start=pygame.mixer.Sound("Resources/sounds/I am notta da mario.ogg"),
+                           pause=pygame.mixer.Sound("Resources/sounds/paused.ogg"))
         self.sounds['click'].set_volume(0.5)
         self.sounds['start'].set_volume(2)
+        self.sounds['pause'].set_volume(5)
 
 
     def update(self):
@@ -212,6 +214,7 @@ class UI():
                     self.sounds['start'].play()
                     self.game_start()
                 case 'Pause':
+                    self.sounds['pause'].play()
                     self.save_state()
                     self.open_menus.append(PauseMenu("Pause"))
                 case 'Return':
